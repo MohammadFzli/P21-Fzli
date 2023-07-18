f59cc0d2d09303586b3016668f2fb1dd1b36758d
Mohammad_Fazeli <mohammadfazeli.developer@gmail.com>
2023-07-18 17:51:20 +0330

    first-js-code

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let numOne = +prompt("Enter The First Number:");
+let numTwo = +prompt("Enter The Second Number:");
+
+sumOfNumbers = numOne + numTwo;
+minusOfNumbers = numOne - numTwo;
+multiOfNumbers = numOne * numTwo;
+divisionOfNumbers = numOne / numTwo;
+remainderOfNumbers = numOne % numTwo;
+
+document.writeln("First Number { ",numOne," }"," Second Number { ",numTwo," }",);
+document.writeln("Sum of Numbers= ",sumOfNumbers,);
+document.writeln(" Minus of Numbers= ",minusOfNumbers,);
+document.writeln(" Multiplication of Numbers= ",multiOfNumbers,);
+document.writeln(" Division of Numbers= ",divisionOfNumbers,);
+document.writeln(" Remainder of Numbers= ",remainderOfNumbers,);
